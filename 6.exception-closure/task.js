1053bf5b2b06c040c47d0520e4490972fbd3f2a3
katherine <ek.gvozd@yandex.ru>
2023-10-17 19:35:51 +0300

    ДЗ по 6 уроку - готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,59 @@
-﻿
+﻿// задание 1
+function parseCount(num) {
+    let result = Number.parseFloat(num);
+    if (Number.isNaN(result)) {
+        throw new Error("Невалидное значение");
+    } else {
+        return result;
+    }
+}
+
+function validateCount(count) {
+    try {
+        return parseCount(count);
+    } catch (err) {
+        return err;
+    }
+}
+
+// задание 2
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        let sumWithoutC = this.sideA + this.sideB;
+        let sumWithoutB = this.sideA + this.sideC;
+        let sumWithoutA = this.sideB + this.sideC;
+        if (this.sideA > sumWithoutA || this.sideB > sumWithoutB || this.sideC > sumWithoutC) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    get area() {
+        let p = this.perimeter * 0.5;
+        let areaNum = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)).toFixed(3);
+        return Number(areaNum); 
+    }
+
+}
+
+function getTriangle(num1, num2, num3) {
+    try {
+        return new Triangle(num1, num2, num3);
+    } catch (err) {
+        return {
+            get perimeter() {
+              return "Ошибка! Треугольник не существует";
+            },
+
+            get area() {
+              return "Ошибка! Треугольник не существует";
+            } 
+        }
+    }
+}
